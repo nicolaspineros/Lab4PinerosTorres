6b0b9936641e4a3b03547676dd9e4b9c825234e9
Nicolas <onpc1224@gmail.com>
2021-02-13 09:07:25 -0500

    implementacion pruebas

--- src/test/java/GameScoreTest.java
+++ src/test/java/GameScoreTest.java
@@ -1,8 +1,21 @@
 /*
-Clases de Equivalencia
-Clase 1: 
-*/
+ Clases de Equivalencia:
+Clase 1: parametros >= 0
+Clase 2: 0 < puntaje < 500 , y que se cumpla Clase 1
+ */
+import org.junit.*;
+import hangman.model.*;
 
 public class GameScoreTest {
+   
     
+    @Test
+    public void validarParametros(){
+        
+    }
+    
+    @Test
+    public void validarPuntaje(){
+    
+    }
 }
